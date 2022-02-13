e3fd64b3199a912d0a49670ea8c392f77725b185
Kamil Hojka <kamhojka@gmail.com>
2022-02-13 15:16:43 +0100

    Add update note view and url in the Django backend

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,7 +9,6 @@ from .serializers import NoteSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
-
     routes = [
         {
             'Endpoint': '/notes/',
@@ -42,13 +41,12 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-
     return Response(routes)
 
 
 @api_view(['GET'])
 def getNotes(request):
-    notes = Note.objects.all()
+    notes = Note.objects.all().order_by('-updated')
     serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
 
@@ -57,4 +55,16 @@ def getNotes(request):
 def getNote(request, pk):
     note = Note.objects.get(id=pk)
     serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance=note, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
     return Response(serializer.data)
