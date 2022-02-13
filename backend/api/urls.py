6d8bcb76f2de4c1f2b7fe23ab61dc11a09e34bac
Kamil Hojka <kamhojka@gmail.com>
2022-02-13 14:21:43 +0100

    Update note url in the Django backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('notes/', views.getNotes, name="notes"),
-    path('note/<str:pk>/', views.getNote, name="note"),
+    path('notes/<str:pk>/', views.getNote, name="note"),
 ]
