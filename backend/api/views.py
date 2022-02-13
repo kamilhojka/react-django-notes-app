436de1a184d4988bc54b4f43fe2f1ca63a634110
Kamil Hojka <kamhojka@gmail.com>
2022-02-13 15:50:22 +0100

    Add create note view and url in the Django backend

--- backend/api/views.py
+++ backend/api/views.py
@@ -58,6 +58,16 @@ def getNote(request, pk):
     return Response(serializer.data)
 
 
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+    note = Note.objects.create(
+        body=data['body']
+    )
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
 @api_view(['PUT'])
 def updateNote(request, pk):
     data = request.data
